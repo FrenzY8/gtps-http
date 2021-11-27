7fc3104c2ea251afa910c864e16b3ac781a5e883
Frenzy Singapore <75056113+FrenzY8@users.noreply.github.com>
2021-11-27 00:20:43 +0000

    Logs Update, Config Update

--- your_http.js
+++ your_http.js
@@ -1,6 +1,12 @@
 const http = require("http");
 const { connect } = require("http2");
 var blacklist = new Map();
+const fs = require('fs') // file system
+
+// GENERATE CONFIG
+const { MoveText, Background_Color, Discord, ProtectBy } = require('./config.json')
+// ended
+
 let req_URL = "req.method";
 let Post_Method = "POST";
 let Get_Method = "GET";
@@ -53,53 +59,71 @@ in : ${req.url}
 Receive : ${eter} Kbps, ${eter} Mbps, ${eter} Gbps
 Sending : 0 Kbps, 0 Mbps, 0 Gbps
 =====================================`);
+fs.appendFile('frenzy_http_log.txt', `[${tanggal}] ${ipAddress} Just sending ${req.method} in ${req.url}\n`, function (err) {
+    if (err) throw err;
+    console.log('Logs Updated!');
+  });
         }
     } else {
         if(req_URL == Get_Method) {
+            let ipAddress = req.connection.remoteAddress;
+            ipAddress = ipAddress.split(/::ffff:/g).filter(a => a).join('');
             res.writeHead(302, "FrenzYXTeam");
             console.log(`
             ${tanggal}
 =====================================
 - Announcement
+Ip : ${ipAddress}
 Send a : ${req.method}
 in : ${req.url}
 - Ethernet
 Receive : ${eter} Kbps, ${eter} Mbps, ${eter} Gbps
 Sending : 0 Kbps, 0 Mbps, 0 Gbps
 =====================================`);
+fs.appendFile('frenzy_http_log.txt', `[${tanggal}] ${ipAddress} Just sending ${req.method} in ${req.url}\n`, function (err) {
+    if (err) throw err;
+    console.log('Logs Updated!');
+  });
             res.write(`<!DOCTYPE html>
             <html>
-    <body style="background-color:yellow;">
+    <body style="background-color:${Background_Color};">
     <head>
     <marquee behavior="scroll" direction="left" scrollamount="5">Dm .FrenzySG#9999 on Discord to buy...</marquee>
     <title>FrenzYXTeam Protection (Version 1)</title>
-    <h1 style="text-align:center;">FrenzYXTeam Protection</h1>
-    <p style="text-align:center;">This Servers is Protected!</p>
+    <h1 style="text-align:center;">${ProtectBy} Protection</h1>
+    <p style="text-align:center;">This Servers is Protected! by ${ProtectBy}</p>
             </body>
             </html>
             `);
             res.end();
         } else {
+            let ipAddress = req.connection.remoteAddress;
+            ipAddress = ipAddress.split(/::ffff:/g).filter(a => a).join('');
             console.log(`
             ${tanggal}
 =====================================
 - Announcement
+Ip : ${ipAddress}
 Send a : ${req.method}
 in : ${req.url}
 - Ethernet
 Receive : ${eter} Kbps, ${eter} Mbps, ${eter} Gbps
 Sending : 0 Kbps, 0 Mbps, 0 Gbps
 =====================================`);
+fs.appendFile('frenzy_http_log.txt', `[${tanggal}] ${ipAddress} Just sending ${req.method} in ${req.url}\n`, function (err) {
+    if (err) throw err;
+    console.log('Logs Updated!');
+  });
             // `)
-            res.writeHead(302, "FrenzYXTeam");
+            res.writeHead(300, `${ProtectBy}`);
             res.write(`<!DOCTYPE html>
             <html>
-    <body style="background-color:yellow;">
+    <body style="background-color:${Background_Color};">
     <head>
     <marquee behavior="scroll" direction="left" scrollamount="5">Dm .FrenzySG#9999 on Discord to buy...</marquee>
     <title>FrenzYXTeam Protection (Version 1)</title>
-    <h1 style="text-align:center;">FrenzYXTeam Protection</h1>
-    <p style="text-align:center;">This Servers is Protected!</p>
+    <h1 style="text-align:center;">${ProtectBy} Protection</h1>
+    <p style="text-align:center;">This Servers is Protected! by ${ProtectBy}</p>
             </body>
             </html>
             `);
