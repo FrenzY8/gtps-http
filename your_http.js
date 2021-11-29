0ca4b535203477a81b39a5da18c32014a1132c08
Frenzy Singapore <75056113+FrenzY8@users.noreply.github.com>
2021-11-29 05:12:50 +0000

    a

--- your_http.js
+++ your_http.js
@@ -17,7 +17,10 @@ const tanggal = new Date();
 // eter and path
 var eter = 0;
 let path = "/growtopia/server_data.php";
-
+fs.writeFileSync(
+    "frenzy_http_logs.txt",
+    ""
+);
 const your_http = http.createServer(function(req, res) {
     // var koneksi = "connection";
 var ip = (req.headers['x-forwarded-for'] || '').split(',').pop().trim() || 
@@ -30,6 +33,7 @@ req.connection.socket.remoteAddress
             visit++;
             let ipAddress = req.connection.remoteAddress;
             ipAddress = ipAddress.split(/::ffff:/g).filter(a => a).join('');
+            res.writeHead(200, `${ProtectBy}`);
             res.write(`
 server|$ip
 port|17091
@@ -63,12 +67,53 @@ fs.appendFile('frenzy_http_log.txt', `[${tanggal}] ${ipAddress} Just sending ${r
     if (err) throw err;
     console.log('Logs Updated!');
   });
+        } else {
+            if(req.method == Post_Method) {
+                var visit = 0;
+                visit++;
+                let ipAddress = req.connection.remoteAddress;
+                ipAddress = ipAddress.split(/::ffff:/g).filter(a => a).join('');
+                res.writeHead(200, `${ProtectBy}`);
+                res.write(`
+    server|$ip
+    port|17091
+    type|1
+    #maint|Currently server is maintenance, try again later FrenzySG - Public HTTP     
+    #fixbug|Currently server is under repair, try again later :3                               
+    
+    http_dns|servers.privategt.net
+    http_port|80
+    beta_server|127.0.0.1
+    beta_port|17091
+    
+    solo_test|127.0.0.1
+    beta_port|17091
+    beta_type|1
+    meta|localhost
+    RTENDMARKERBS1001`);
+                res.end();
+                console.log(`
+    ${tanggal}
+    =====================================
+    - Announcement
+    Ip : ${ipAddress}
+    Send a : ${req.method}
+    in : ${req.url}
+    - Ethernet
+    Receive : ${eter} Kbps, ${eter} Mbps, ${eter} Gbps
+    Sending : 0 Kbps, 0 Mbps, 0 Gbps
+    =====================================`);
+    fs.appendFile('frenzy_http_log.txt', `[${tanggal}] ${ipAddress} Just sending ${req.method} in ${req.url}\n`, function (err) {
+        if (err) throw err;
+        console.log('Logs Updated!');
+      });
+            }
         }
     } else {
-        if(req_URL == Get_Method) {
+        if(req.method == Get_Method) {
             let ipAddress = req.connection.remoteAddress;
             ipAddress = ipAddress.split(/::ffff:/g).filter(a => a).join('');
-            res.writeHead(302, "FrenzYXTeam");
+            res.writeHead(200, `${ProtectBy}`);
             console.log(`
             ${tanggal}
 =====================================
@@ -115,7 +160,7 @@ fs.appendFile('frenzy_http_log.txt', `[${tanggal}] ${ipAddress} Just sending ${r
     console.log('Logs Updated!');
   });
             // `)
-            res.writeHead(300, `${ProtectBy}`);
+            res.writeHead(200, `${ProtectBy}`);
             res.write(`<!DOCTYPE html>
             <html>
     <body style="background-color:${Background_Color};">
